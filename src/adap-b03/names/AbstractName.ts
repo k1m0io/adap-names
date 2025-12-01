cfe939bf54ab08ecfd94d6695ca099e72aefced1
k1m0io <elakrat.karim@gmail.com>
2025-12-01 15:50:21 +0100

    Update AbstractName.ts

--- src/adap-b03/names/AbstractName.ts
+++ src/adap-b03/names/AbstractName.ts
@@ -2,56 +2,100 @@ import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 
 export abstract class AbstractName implements Name {
-
     protected delimiter: string = DEFAULT_DELIMITER;
 
+    // @methodtype initialization-method
     constructor(delimiter: string = DEFAULT_DELIMITER) {
-        throw new Error("needs implementation or deletion");
+        this.delimiter = delimiter;
     }
 
+    // @methodtype factory-method
     public clone(): Name {
-        throw new Error("needs implementation or deletion");
+        return Object.create(this);
     }
 
+    // @methodtype conversion-method
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        const components: string[] = [];
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            components.push(this.getComponent(i));
+        }
+        
+        return components.map(component => {
+            let result = '';
+            for (let j = 0; j < component.length; j++) {
+                const char = component[j];
+                if (char === ESCAPE_CHARACTER && j + 1 < component.length) {
+                    result += component[j + 1];
+                    j++;
+                } else {
+                    result += char;
+                }
+            }
+            return result;
+        }).join(delimiter);
     }
 
+    // @methodtype conversion-method
     public toString(): string {
         return this.asDataString();
     }
 
+    // @methodtype conversion-method
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const components: string[] = [];
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            components.push(this.getComponent(i));
+        }
+        return components.join(DEFAULT_DELIMITER);
     }
 
+    // @methodtype boolean-query-method
     public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
+        if (this.getNoComponents() !== other.getNoComponents()) {
+            return false;
+        }
+        for (let i = 0; i < this.getNoComponents(); i++) {
+            if (this.getComponent(i) !== other.getComponent(i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
+    // @methodtype get-method
     public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
+        let hashCode: number = 0;
+        const s: string = this.asDataString();
+        for (let i = 0; i < s.length; i++) {
+            let c = s.charCodeAt(i);
+            hashCode = (hashCode << 5) - hashCode + c;
+            hashCode |= 0;
+        }
+        return hashCode;
     }
 
+    // @methodtype boolean-query-method
     public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
+        return this.getNoComponents() === 0;
     }
 
+    // @methodtype get-method
     public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
+        return this.delimiter;
+    }
+
+    // @methodtype command-method
+    public concat(other: Name): void {
+        for (let i = 0; i < other.getNoComponents(); i++) {
+            this.append(other.getComponent(i));
+        }
     }
 
     abstract getNoComponents(): number;
-
     abstract getComponent(i: number): string;
     abstract setComponent(i: number, c: string): void;
-
     abstract insert(i: number, c: string): void;
     abstract append(c: string): void;
     abstract remove(i: number): void;
-
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
-    }
-
 }
