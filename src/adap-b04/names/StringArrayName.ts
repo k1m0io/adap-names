0ebcc01af1e350dd1346f8ef0b740d1222910679
k1m0io <elakrat.karim@gmail.com>
2025-12-01 15:59:34 +0100

    Update StringArrayName.ts

--- src/adap-b04/names/StringArrayName.ts
+++ src/adap-b04/names/StringArrayName.ts
@@ -1,69 +1,118 @@
 import { DEFAULT_DELIMITER, ESCAPE_CHARACTER } from "../common/Printable";
 import { Name } from "./Name";
 import { AbstractName } from "./AbstractName";
+import { IllegalArgumentException } from "../common/IllegalArgumentException";
+import { MethodFailedException } from "../common/MethodFailedException";
 
 export class StringArrayName extends AbstractName {
-
     protected components: string[] = [];
 
+    // @methodtype initialization-method
     constructor(source: string[], delimiter?: string) {
-        super();
-        throw new Error("needs implementation or deletion");
+        super(delimiter);
+        
+        // Preconditions
+        if (source === null || source === undefined) {
+            throw new IllegalArgumentException("Source array cannot be null or undefined");
+        }
+        
+        this.components = [...source];
+        this.assertClassInvariants();
     }
 
-    public clone(): Name {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public asDataString(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEqual(other: Name): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getHashCode(): number {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public isEmpty(): boolean {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public getDelimiterCharacter(): string {
-        throw new Error("needs implementation or deletion");
-    }
-
+    // @methodtype get-method
     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
+        return this.components.length;
     }
 
+    // @methodtype get-method
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        // Preconditions
+        this.assertIsValidIndex(i, false);
+        
+        const result = this.components[i];
+        
+        // Postconditions
+        MethodFailedException.assertCondition(result !== null && result !== undefined, "getComponent returned null");
+        
+        this.assertClassInvariants();
+        return result;
     }
 
-    public setComponent(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype set-method
+    public setComponent(i: number, c: string): void {
+        // Preconditions
+        this.assertIsValidIndex(i, false);
+        this.assertIsValidComponent(c);
+        
+        this.components[i] = c;
+        
+        // Postconditions
+        MethodFailedException.assertCondition(this.components[i] === c, "setComponent failed to set component");
+        
+        this.assertClassInvariants();
     }
 
-    public insert(i: number, c: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype command-method
+    public insert(i: number, c: string): void {
+        // Preconditions
+        this.assertIsValidIndex(i, true);
+        this.assertIsValidComponent(c);
+        
+        const oldNoComponents = this.getNoComponents();
+        
+        this.components.splice(i, 0, c);
+        
+        // Postconditions
+        MethodFailedException.assertCondition(
+            this.getNoComponents() === oldNoComponents + 1,
+            "insert did not increase number of components by 1"
+        );
+        MethodFailedException.assertCondition(
+            this.getComponent(i) === c,
+            "insert did not place component at correct position"
+        );
+        
+        this.assertClassInvariants();
     }
 
-    public append(c: string) {
-        throw new Error("needs implementation or deletion");
+    // @methodtype command-method
+    public append(c: string): void {
+        // Preconditions
+        this.assertIsValidComponent(c);
+        
+        const oldNoComponents = this.getNoComponents();
+        
+        this.components.push(c);
+        
+        // Postconditions
+        MethodFailedException.assertCondition(
+            this.getNoComponents() === oldNoComponents + 1,
+            "append did not increase number of components by 1"
+        );
+        MethodFailedException.assertCondition(
+            this.getComponent(this.getNoComponents() - 1) === c,
+            "append did not place component at end"
+        );
+        
+        this.assertClassInvariants();
     }
 
-    public remove(i: number) {
-        throw new Error("needs implementation or deletion");
-    }
-
-    public concat(other: Name): void {
-        throw new Error("needs implementation or deletion");
+    // @methodtype command-method
+    public remove(i: number): void {
+        // Preconditions
+        this.assertIsValidIndex(i, false);
+        
+        const oldNoComponents = this.getNoComponents();
+        
+        this.components.splice(i, 1);
+        
+        // Postconditions
+        MethodFailedException.assertCondition(
+            this.getNoComponents() === oldNoComponents - 1,
+            "remove did not decrease number of components by 1"
+        );
+        
+        this.assertClassInvariants();
     }
 }
