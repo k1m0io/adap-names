8671a98e5f8d07bb391df1a91fd61613cae909af
k1m0io <elakrat.karim@gmail.com>
2025-12-08 16:12:15 +0100

    Update Directory.ts

--- src/adap-b05/files/Directory.ts
+++ src/adap-b05/files/Directory.ts
@@ -1,4 +1,6 @@
 import { Node } from "./Node";
+import { InvalidStateException } from "../common/InvalidStateException";
+import { ServiceFailureException } from "../common/ServiceFailureException";
 
 export class Directory extends Node {
     protected childNodes: Set<Node> = new Set<Node>();
@@ -23,19 +25,33 @@ export class Directory extends Node {
      * Override findNodes to search recursively through children
      */
     public findNodes(bn: string): Set<Node> {
-        const result: Set<Node> = new Set<Node>();
-        
-        // Check if this directory matches
-        if (this.getBaseName() === bn) {
-            result.add(this);
+        try {
+            const result: Set<Node> = new Set<Node>();
+            
+            // ERROR DETECTION: Check for invalid state
+            const baseName = this.getBaseName();
+            if (baseName === "") {
+                throw new InvalidStateException("Directory has empty basename");
+            }
+            
+            // Check if this directory matches
+            if (baseName === bn) {
+                result.add(this);
+            }
+            
+            // Search all children recursively
+            for (const child of this.childNodes) {
+                const childMatches = child.findNodes(bn);
+                childMatches.forEach(node => result.add(node));
+            }
+            
+            return result;
+        } catch (ex) {
+            // At service boundary: wrap in ServiceFailureException
+            if (ex instanceof InvalidStateException) {
+                throw new ServiceFailureException("findNodes failed in directory", ex);
+            }
+            throw ex;
         }
-        
-        // Search all children recursively
-        for (const child of this.childNodes) {
-            const childMatches = child.findNodes(bn);
-            childMatches.forEach(node => result.add(node));
-        }
-        
-        return result;
     }
 }
